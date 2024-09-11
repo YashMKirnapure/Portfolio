d3e2fdf36a6421b4b71f1dd75a8f7d91b42b1f80
YashKirnapure77 <yashkirnapure777@gmail.com>
2024-09-11 16:32:36 +0530

    Navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import photo from "../../public/photo.avif";
+import { CgMenu } from "react-icons/cg";
+import { IoCloseSharp } from "react-icons/io5";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  const navItems = [
+    {
+      id: 1,
+      text: "Home",
+    },
+    {
+      id: 2,
+      text: "About",
+    },
+    {
+      id: 3,
+      text: "Portfolio",
+    },
+    {
+      id: 4,
+      text: "Experience",
+    },
+    {
+      id: 5,
+      text: "Contact",
+    },
+  ];
+
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 ">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex space-x-2">
+            <img src={photo} className="h-12 w-12 rounded-full" alt="" />
+            <h1 className="font-semibold text-xl cursor-pointer">
+              Yash Kirnapure
+              <p className="text-sm text-fuchsia-500 ">Web Developer</p>
+            </h1>
+          </div>
+
+          {/* Laptop Navbar */}
+          <div>
+            <ul className="hidden md:flex space-x-8">
+              {navItems.map(({ id, text }) => (
+                <li className='hover:scale-105 duration-200 cursor-pointer'
+                key={id}>{text}</li>
+              ))}
+            </ul>
+            <div onClick={() => setMenu(!menu)} className="md:hidden">
+              {menu ? <CgMenu size={24} /> : <IoCloseSharp size={24} />}
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Navbar */}
+        {menu && (
+          <div className="">
+            <ul className="md:hidden flex flex-col h-screen items-center justify-center space-y-3 text-xl">
+              {navItems.map(({ id, text }) => (
+                <li className='hover:scale-105 duration-200 font-semibold cursor-pointer' key={id}>{text}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
